1f4e4ba50e78e76acc2e6efd685ff16b4be24a1f
venh <conf312@naver.com>
2022-10-23 20:53:28 +0900

    [modify] container color

--- src/components/bus/Detail.js
+++ src/components/bus/Detail.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
 import { useParams } from "react-router-dom";
-import { bus_blue, bus_green, bus_yellow } from '../../images';
+import { bus_red, bus_blue, bus_green, bus_yellow, bus_black } from '../../images';
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import * as AxiosUtil from '../../lib/AxiosUtil';
 import XMLParser from 'react-xml-parser';
@@ -24,7 +24,6 @@ function Detail() {
     setDist(params.dist);
 
     let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + process.env.REACT_APP_SEOUL_STATION_SERVICE_KEY;
-    console.log("params.stationId : "+params.stationId)
     queryParams += '&' + encodeURIComponent('arsId') + '=' + encodeURIComponent(params.arsId);
     queryParams += '&' + encodeURIComponent('resultType') + '=' + encodeURIComponent('json');
     AxiosUtil.send("GET", "/getSeoulStation/getLowStationByUid" + queryParams, "", "", (e) => {
@@ -37,16 +36,14 @@ function Detail() {
   }, []);
 
   function getBusApiCall() {
-    let queryParams2 = '?' + encodeURIComponent('serviceKey') + '=' + process.env.REACT_APP_SEOUL_STATION_SERVICE_KEY;
-    queryParams2 += '&' + encodeURIComponent('stationId') + '=' + encodeURIComponent(params.stationId);
-    AxiosUtil.send("GET", "/getBusarrivalservice/getBusArrivalList" + queryParams2, "", "", (e) => {
-      console.log(e)
+    let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + process.env.REACT_APP_SEOUL_STATION_SERVICE_KEY;
+    queryParams += '&' + encodeURIComponent('stationId') + '=' + encodeURIComponent(params.stationId);
+    AxiosUtil.send("GET", "/getBusarrivalservice/getBusArrivalList" + queryParams, "", "", (e) => {
       const dataArr = new XMLParser().parseFromString(e).children[2];
       if (dataArr !== null) { 
-        dataArr.children.forEach(function(item){
-          console.log(item)
-          var obj = new Object();
-          item.children.forEach(function(detail){
+        dataArr.children.forEach(function(item) {
+          let obj = new Object();
+          item.children.forEach(function(detail) {
             obj[detail.name] = detail.value;
             if (detail.name  === "routeId") {
               let queryParams2 = '?' + encodeURIComponent('serviceKey') + '=' + process.env.REACT_APP_SEOUL_STATION_SERVICE_KEY;
@@ -54,13 +51,15 @@ function Detail() {
               AxiosUtil.send("GET", "/getBusrouteservice/getBusRouteInfoItem" + queryParams2, "", "", (e) => {
                 const dataArr = new XMLParser().parseFromString(e).children[2];
                 if (dataArr !== null) { 
-                  dataArr.children.forEach(function(item){
-                    console.log(item)
+                  dataArr.children.forEach(function(item) {
                     item.children.forEach(function(detail){
                       if (detail.name === "routeName") {
                         obj[detail.name] = detail.value;
                       }
                       if (detail.name === "startStationName") {
+                        obj[detail.name] = detail.value;
+                      }
+                      if (detail.name === "routeTypeCd") {
                         obj[detail.name] = detail.value;
                       }
                     });
@@ -75,8 +74,8 @@ function Detail() {
     });
   }
 
-  function getDetailRoute(stationNo, busRouteId) {
-    window.location.href = "/detail-route/" + stationNo + "/" + busRouteId;
+  function getDetailRoute(stationNo, busRouteId, routeTypeCd) {
+    window.location.href = "/detail-route/" + stationNo + "/" + busRouteId + "/" + routeTypeCd;
   }
 
   function GetBusRouteAbrv(props) {
@@ -93,7 +92,7 @@ function Detail() {
       return (
         <Card.Title>
           <img src={bus_green} alt="bus_green" width={25}></img>  
-          <span className="ft-gm text-success" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
+          <span className="ft-gm fw-bold text-success" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
           <span className="text-muted"> | </span>  
           <span className="ft-gm text-muted">{props.adirection} 방면</span>
         </Card.Title>
@@ -101,8 +100,57 @@ function Detail() {
     } else {
       return (
         <Card.Title>
+          <img src={bus_black} alt="bus_black" width={25}></img>  
+          <span className="ft-gm fw-bold" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
+          <span className="text-muted"> | </span>  
+          <span className="ft-gm text-muted"> | {props.adirection} 방면</span>
+        </Card.Title>
+      );
+    }
+  }
+
+  function GetGBusRouteAbrv(props) {
+    if (props.routeTypeCd === "12" || props.routeTypeCd === "22" || props.routeTypeCd === "42" || props.routeTypeCd === "52") {
+      return (
+        <Card.Title>
+          <img src={bus_blue} alt="bus_blue" width={25}></img>
+          <span className="ft-gm fw-bold text-primary" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
+          <span className="text-muted"> | </span>          
+          <span className="ft-gm text-muted">{props.adirection} 방면</span>
+        </Card.Title>
+      );
+    } else if (props.routeTypeCd === "11" || props.routeTypeCd === "14" || props.routeTypeCd === "16" || props.routeTypeCd === "21") {
+      return (
+        <Card.Title>
+          <img src={bus_red} alt="bus_red" width={25}></img>  
+          <span className="ft-gm fw-bold text-danger" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
+          <span className="text-muted"> | </span>  
+          <span className="ft-gm text-muted">{props.adirection} 방면</span>
+        </Card.Title>
+      );
+    } else if (props.routeTypeCd === "13" || props.routeTypeCd === "23" || props.routeTypeCd === "43" || props.routeTypeCd === "53") {
+      return (
+        <Card.Title>
+          <img src={bus_green} alt="bus_green" width={25}></img>  
+          <span className="ft-gm fw-bold text-success" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
+          <span className="text-muted"> | </span>  
+          <span className="ft-gm text-muted">{props.adirection} 방면</span>
+        </Card.Title>
+      );
+    } else if (props.routeTypeCd === "30") {
+      return (
+        <Card.Title>
           <img src={bus_yellow} alt="bus_yellow" width={25}></img>  
-          <span className="ft-gm" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
+          <span className="ft-gm fw-bold text-warning" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
+          <span className="text-muted"> | </span>  
+          <span className="ft-gm text-muted"> | {props.adirection} 방면</span>
+        </Card.Title>
+      );
+    } else {
+      return (
+        <Card.Title>
+          <img src={bus_black} alt="bus_black" width={25}></img>  
+          <span className="ft-gm fw-bold" style={{fontSize:"22px", marginLeft:"10px"}}>{props.no}</span>
           <span className="text-muted"> | </span>  
           <span className="ft-gm text-muted"> | {props.adirection} 방면</span>
         </Card.Title>
@@ -125,7 +173,7 @@ function Detail() {
         </div>
         {stationList.length > 0 && 
           stationList.map((data, idx) => (
-            <Card className="mt-3" key={idx} onClick={() => getDetailRoute(data.rtNm, data.busRouteId)}>
+            <Card className="mt-3" key={idx} onClick={() => getDetailRoute(data.rtNm, data.busRouteId, null)}>
               <Card.Body>
                 <GetBusRouteAbrv no={data.busRouteAbrv} adirection={data.adirection} />
                 <Card.Subtitle className="ft-gm mt-3 text-danger">{data.arrmsg1}</Card.Subtitle>
@@ -136,14 +184,9 @@ function Detail() {
         } 
         {gstationList.length > 0 &&
           gstationList.map((data, idx) => (
-            <Card className="mt-3" key={idx} onClick={() => getDetailRoute(data.routeName, data.routeId)}>
+            <Card className="mt-3" key={idx} onClick={() => getDetailRoute(data.routeName, data.routeId, data.routeTypeCd)}>
               <Card.Body>
-                <Card.Title>
-                  <img src={bus_green} alt="bus_green" width={25}></img>  
-                  <span className="ft-gm" style={{fontSize:"22px"}}> {data.routeName}</span>
-                  <span className="text-muted"> | </span>  
-                  <span className="ft-gm text-muted">{data.startStationName} 방면</span>
-                </Card.Title>
+                <GetGBusRouteAbrv no={data.routeName} adirection={data.startStationName} routeTypeCd={data.routeTypeCd} />
                 <Card.Subtitle className="ft-gm mt-3 text-danger">{data.predictTime1}분후[{data.locationNo1}번째 전] / {data.remainSeatCnt1}석</Card.Subtitle>
                 <Card.Subtitle className="ft-gm mt-2 text-muted">{data.predictTime2}분후[{data.locationNo2}번째 전] / {data.remainSeatCnt2}석</Card.Subtitle>
               </Card.Body>
